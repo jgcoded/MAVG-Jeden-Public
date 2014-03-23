a28a2212f8fab456aefb07f767c4bb2c4247c115
jgcoded <jgutierrezleyva@mail.valenciacollege.edu>
2014-03-23 16:06:03 -0400

    AudioSource: use property fields for sound properties

--- src/AudioSource.cs
+++ src/AudioSource.cs
@@ -10,9 +10,20 @@ namespace Project_Jeden.src
     {
         int sourceHandle;
 
+        /* Sound properties */
+        bool loopSound;
+        float soundGain;
+        float maximumSoundGain;
+        Vector2D soundPosition;
+        Vector2D soundVelocity;
+        Vector2D soundDirection;
+
         public AudioSource()
         {
             ALError e;
+
+            loopSound = false;
+            soundPosition = soundDirection = soundVelocity = new Vector2D();
 
             sourceHandle = AL.GenSource();
             
@@ -21,7 +32,7 @@ namespace Project_Jeden.src
                     AL.GetErrorString(e));
         }
 
-        public void AttachAudio(AudioFile file)
+        public void AttachAudioFile(AudioFile file)
         {
             AL.GetError();
             ALError e;
@@ -33,52 +44,129 @@ namespace Project_Jeden.src
                     file.path + "; " + AL.GetErrorString(e));
         }
 
-        public bool Play()
+        public void PlaySound()
         {
             AL.GetError();
             AL.SourcePlay(sourceHandle);
             
             ALError e;
             if ((e = AL.GetError()) != ALError.NoError)
-            {
                 Console.WriteLine("AL Error: error playing source: " + 
                     AL.GetErrorString(e));
-                return false;
-            }
-
-            return true;
         }
 
-        public bool Stop()
+        public void StopSound()
         {
             AL.GetError();
             AL.SourceStop(sourceHandle);
 
             if (AL.GetError() != ALError.NoError)
-            {
                 Console.WriteLine("AL Error: error playing source");
-                return false;
-            }
-
-            return true;
         }
 
-        public bool IsPlaying 
+        public bool IsPlayingSound
         {
             get 
             { 
                 int state;
                 AL.GetSource(sourceHandle, ALGetSourcei.SourceState, out state);
                 return (ALSourceState)state == ALSourceState.Playing;
+            }
+        }
 
+        public bool LoopSound
+        {
+            get
+            {
+                return loopSound;
+            }
+            set
+            {
+                loopSound = value;
+                AL.Source(sourceHandle, ALSourceb.Looping, loopSound);
             }
         }
 
         public int GetSourceHandle { get { return sourceHandle; } }
 
-        public void SetPosition(float x, float y, float z)
+        public Vector2D SoundPosition
         {
-            AL.Source(sourceHandle, ALSource3f.Position, x, y, z);
+            get
+            {
+                return soundPosition;
+            }
+            set
+            {
+                soundPosition = value;
+
+                AL.Source(sourceHandle, 
+                    ALSource3f.Position,
+                    soundPosition.x,
+                    soundPosition.y, 
+                    0);
+            }
+        }
+
+        public Vector2D SoundVelocity
+        {
+            get
+            {
+                return soundVelocity;
+            }
+            set
+            {
+                soundVelocity = value;
+
+                AL.Source(sourceHandle, 
+                    ALSource3f.Velocity, 
+                    soundVelocity.x, 
+                    soundVelocity.y, 
+                    0);
+            }
+        }
+
+        public Vector2D SoundDirection
+        {
+            get
+            {
+                return soundDirection;
+            }
+            set
+            {
+                soundDirection = value;
+
+                AL.Source(sourceHandle, 
+                    ALSource3f.Direction, 
+                    soundDirection.x, 
+                    soundDirection.y, 
+                    0);
+            }
+        }
+
+        public float SoundGain
+        {
+            get
+            {
+                return soundGain;
+            }
+            set
+            {
+                soundGain = value;
+                AL.Source(sourceHandle, ALSourcef.Gain, soundGain);
+            }
+        }
+
+        public float MaximumSoundGain
+        {
+            get
+            {
+                return maximumSoundGain;
+            }
+            set
+            {
+                maximumSoundGain = value;
+                AL.Source(sourceHandle, ALSourcef.MaxGain, maximumSoundGain);
+            }
         }
 
         ~AudioSource()
